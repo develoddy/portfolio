6938e4dbefbada8ab4abc9ba2a60ea666df59ee8
Eddy Lujan <eddylujann@gmail.com>
2024-04-23 16:38:13 +0200

    subir de prueba un camnio de auth

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -25,7 +25,6 @@ class AuthenticatedSessionController extends Controller
      */
     public function store(LoginRequest $request): RedirectResponse
     {
-        //dd("xx");
         $request->authenticate();
        
 
