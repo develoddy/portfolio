b3cbf650d1293c7873a0c709f023ddef9a139eb3
Eddy Lujan <eddylujann@gmail.com>
2024-05-03 09:52:15 +0200

    icon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,7 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-        <link rel="shortcut icon" type="image/x-icon" href="{{ Vite::asset('resources/imgs/faviconld.ico') }}">
+        <link rel="shortcut icon" type="image/x-icon" href="{{ Vite::asset('resources/imgs/favicon.ico') }}">
         @vite(['resources/js/app.js', 'resources/sass/app.scss'])
         
     </head>
