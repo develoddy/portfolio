bffacb7b8370873b70c97b2c3055e45419a46e04
EVIDENZE\e.lujan <e.lujan@evidenze.com>
2025-02-13 23:04:28 +0100

    fix

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -46,7 +46,7 @@ class AdminPanelProvider extends PanelProvider
         ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
         ->widgets([
             Widgets\AccountWidget::class,
-            Widgets\FilamentInfoWidget::class,
+            //Widgets\FilamentInfoWidget::class,
         ])
         ->middleware([
             EncryptCookies::class,
@@ -64,9 +64,11 @@ class AdminPanelProvider extends PanelProvider
         ]);
     }
 
-    // public function register(): void
-    // {
-    //   parent::register();
-    //   FilamentView::registerRenderHook('panels::body.end', fn(): string => Blade::render("@vite('resources/js/app.js')"));
-    // }
+   
+
+    public function register(): void
+    {
+      parent::register();
+      FilamentView::registerRenderHook('panels::body.end', fn(): string => Blade::render("@vite('resources/js/app.js')"));
+    }
 }
