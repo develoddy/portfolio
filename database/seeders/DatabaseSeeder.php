6ce5dfd9927f2dbfd1f4d43b13f400eebb207af0
EVIDENZE\e.lujan <e.lujan@evidenze.com>
2025-02-14 08:06:05 +0100

    remove seeder tcg

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,27 +21,10 @@ class DatabaseSeeder extends Seeder
         // php artisan db:seed --class=MenusTableSeeder
 
 
-        // Creación inicial
-        $this->call(MenusTableSeeder::class);
-        $this->call(MenuItemsTableSeeder::class);
-        $this->call(DataTypesTableSeeder::class);
-        $this->call(DataRowsTableSeeder::class);
-
-        // Roles y permisos
-        $this->call(RolesTableSeeder::class);
-        $this->call(PermissionRoleTableSeeder::class);
-        $this->call(PermissionsTableSeeder::class);
+        
 
         // Usuarios y datos adicionales
         $this->call(UsersTableSeeder::class);
-        $this->call(PagesTableSeeder::class);
-        $this->call(CategoriesTableSeeder::class);
-
-        // Datos específicos de Voyager
-        $this->call(SettingsTableSeeder::class);
-        
-        $this->call(VoyagerDatabaseSeeder::class);
-        $this->call(VoyagerDummyDatabaseSeeder::class);
-        $this->call(TranslationsTableSeeder::class);
+       
     }
 }
