6289002c823a0d2615069bd388dd06d04929b618
EVIDENZE\e.lujan <e.lujan@evidenze.com>
2025-02-17 17:44:04 +0100

    fix vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -14,7 +14,7 @@ export default defineConfig({
             ],
             refresh: [
                 ...refreshPaths,
-                //'app/Livewire/**',
+                'app/Livewire/**',
                 'app/Filament/**'
               ],
         }),
