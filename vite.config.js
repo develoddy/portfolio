51a689aa21783475ac9d9364bbeae59ed643f0fe
EVIDENZE\e.lujan <e.lujan@evidenze.com>
2025-02-17 17:46:55 +0100

    fix vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite';
-import laravel, { refreshPaths } from 'laravel-vite-plugin'; //import laravel from 'laravel-vite-plugin';
+import laravel from 'laravel-vite-plugin'; //import laravel, { refreshPaths } from 'laravel-vite-plugin';
 import path from 'path';
 
 
@@ -12,11 +12,7 @@ export default defineConfig({
                 'resources/js/bootstrap.js',
                 'resources/js/main.js',
             ],
-            refresh: [
-                ...refreshPaths,
-                'app/Livewire/**',
-                'app/Filament/**'
-              ],
+            refresh: true,
         }),
     ],
 
