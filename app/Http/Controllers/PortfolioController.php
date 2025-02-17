f6055dbe2dccc5ba0030b2391e3ebe54835f5d27
EVIDENZE\e.lujan <e.lujan@evidenze.com>
2025-02-17 19:10:33 +0100

    cambiar name metodo de portfoliodetail

--- app/Http/Controllers/PortfolioController.php
+++ app/Http/Controllers/PortfolioController.php
@@ -14,14 +14,14 @@ class PortfolioController extends Controller
             $portfolios = Portfolio::all();
             // Obtener todos los portafolios con sus detalles
 
-            // $portfolios = Portfolio::with('portfolioDetails')->get();
+         
             // Obtener todos los portafolios que tienen al menos un detalle con la categoría "Brand"
-            $brands = Portfolio::whereHas('portfolioDetails', function ($query) {
+            $brands = Portfolio::whereHas('portfolioDetail', function ($query) {
                 $query->where('category', 'Brand');
             })->get();
 
             // Obtener todos los portafolios que tienen al menos un detalle con la categoría "Project"
-            $projects = Portfolio::whereHas('portfolioDetails', function ($query) {
+            $projects = Portfolio::whereHas('portfolioDetail', function ($query) {
                 $query->where('category', 'Project');
             })->get();
             
@@ -35,14 +35,6 @@ class PortfolioController extends Controller
         try {
             $portfolioDetails = PortfolioDetail::where('id', $portfolio_id)->get(); 
             return view('web.portfolio.portfolio-details', compact('portfolioDetails'));
-            
-            /*if (!empty($portfolioDetail)) {
-                $description = implode(', ', $portfolioDetail->pluck('description')->toArray());
-                $title = implode(', ', $portfolioDetail->pluck('title')->toArray());
-                $images =  implode(', ', $portfolioDetail->pluck('image')->toArray()); 
-                $arr_images =  json_decode($images, true);
-            }
-            return view('web.portfolio.portfolio-details', compact('portfolioDetails', 'description', 'title', 'arr_images'));*/
 
         } catch (\Throwable $th) {
             throw $th;
